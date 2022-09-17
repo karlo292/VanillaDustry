0db7abbeeb63ff224320728d5eaa04b02d22c4d5
karlo292 <karlito1501@gmail.com>
2022-09-17 18:02:43 +0200

    Added /discord command

--- src/example/ExamplePlugin.java
+++ src/example/ExamplePlugin.java
@@ -4,42 +4,13 @@ import arc.*;
 import arc.util.*;
 import mindustry.*;
 import mindustry.content.*;
-import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
-import mindustry.net.Administration.*;
-import mindustry.world.blocks.storage.*;
+
 
 public class ExamplePlugin extends Plugin{
 
     //called when game initializes
-    @Override
-    public void init(){
-        //listen for a block selection event
-        Events.on(BuildSelectEvent.class, event -> {
-            if(!event.breaking && event.builder != null && event.builder.buildPlan() != null && event.builder.buildPlan().block == Blocks.thoriumReactor && event.builder.isPlayer()){
-                //player is the unit controller
-                Player player = event.builder.getPlayer();
-
-                //send a message to everyone saying that this player has begun building a reactor
-                Call.sendMessage("[scarlet]ALERT![] " + player.name + " has begun building a reactor at " + event.tile.x + ", " + event.tile.y);
-            }
-        });
-
-        //add a chat filter that changes the contents of all messages
-        //in this case, all instances of "heck" are censored
-        Vars.netServer.admins.addChatFilter((player, text) -> text.replace("heck", "h*ck"));
-
-        //add an action filter for preventing players from doing certain things
-        Vars.netServer.admins.addActionFilter(action -> {
-            //random example: prevent blast compound depositing
-            if(action.type == ActionType.depositItem && action.item == Items.blastCompound && action.tile.block() instanceof CoreBlock){
-                action.player.sendMessage("Example action filter: Prevents players from depositing blast compound into the core.");
-                return false;
-            }
-            return true;
-        });
-    }
 
     //register commands that run on the server
     @Override
@@ -61,13 +32,13 @@ public class ExamplePlugin extends Plugin{
     @Override
     public void registerClientCommands(CommandHandler handler){
 
-        //register a simple reply command
-        handler.<Player>register("reply", "<text...>", "A simple ping command that echoes a player's text.", (args, player) -> {
-            player.sendMessage("You said: [accent] " + args[0]);
+        handler.<Player>register("discord", "Get discord invite link.", (args, player) -> {
+            player.sendMessage("[#FFD700]Discord Invite: https://discord.gg/tXcRKQhksk");
         });
 
-        //register a whisper command which can be used to send other players messages
-        handler.<Player>register("whisper", "<player> <text...>", "Whisper text to another player.", (args, player) -> {
+
+        //register a message command which can be used to send other players messages
+        handler.<Player>register("msg", "<player> <text...>", "Message another player.", (args, player) -> {
             //find player by name
             Player other = Groups.player.find(p -> p.name.equalsIgnoreCase(args[0]));
 
@@ -78,7 +49,8 @@ public class ExamplePlugin extends Plugin{
             }
 
             //send the other player a message, using [lightgray] for gray text color and [] to reset color
-            other.sendMessage("[lightgray](whisper) " + player.name + ":[] " + args[1]);
+            other.sendMessage("[lightgray](message) " + player.name + ":[] " + args[1]);
         });
     }
+
 }
